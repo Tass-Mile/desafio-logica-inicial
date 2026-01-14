d57425720422d99bdbb520646a2fe382c48907f8
Tass-Mile <tassiambrandao@gmail.com>
2026-01-14 11:37:27 -0300

    nome idade

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let nome = "Milene"
+let idade = 22
 
-console.log(nome);
+console.log(`Olá, meu nome é ${nome} e tenho ${idade} de idade`);
+
+console.log(`Daqui a 10 anos eu terei ${idade + 10} anos.`);
